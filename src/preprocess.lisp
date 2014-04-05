fe862c41961e887c037d408aa8887d20e0712fe9
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-05 11:10:46 -0300

    Separate some concerns

--- src/preprocess.lisp
+++ src/preprocess.lisp
@@ -6,16 +6,17 @@
 
 (defparameter +cc+ "gcc")
 
-(defparameter +cmc-lexer-bin+ "/usr/bin/cmc-lexer")
-
 (defparameter +preproc-cmd+
-  (format nil "~A -xc - -E | ~A"
-          +cc+ +cmc-lexer-bin+))
+  (format nil "~A -xc - -E"
+          +cc+))
 
 (defun preprocess (data)
   (multiple-value-bind (stdout stderr exit-code)
       (trivial-shell:shell-command +preproc-cmd+ :input data)
     (when (not (eql exit-code 0))
       (format t "An error occurred during preprocessing:~&")
-      (format t "~A" stderr))
+      (format t "~A" stderr)
+      (sb-ext:quit))
     stdout))
+
+(defparameter +cmc-lexer-bin+ "/usr/bin/cmc-lexer")
