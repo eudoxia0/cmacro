51b008991ef99a598ba52cbeb4e4be8613480781
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-05 11:01:13 -0300

    Add preprocess function

--- src/preprocess.lisp
+++ src/preprocess.lisp
@@ -1,3 +1,9 @@
+(in-package :cl-user)
+(defpackage cmacro.preprocess
+  (:use :cl))
+(in-package :cmacro.preprocess)
+
+
 (defparameter +cc+ "gcc")
 
 (defparameter +cmc-lexer-bin+ "/usr/bin/cmc-lexer")
@@ -5,3 +11,11 @@
 (defparameter +preproc-cmd+
   (format nil "~A -xc - -E | ~A"
           +cc+ +cmc-lexer-bin+))
+
+(defun preprocess (data)
+  (multiple-value-bind (stdout stderr exit-code)
+      (trivial-shell:shell-command +preproc-cmd+ :input data)
+    (when (not (eql exit-code 0))
+      (format t "An error occurred during preprocessing:~&")
+      (format t "~A" stderr))
+    stdout))
