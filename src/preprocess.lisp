071da5032b72e31278f4664f56d856f1020a882c
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-05 15:17:49 -0300

    Fix macro, change definition of lexer binary pathname

--- src/preprocess.lisp
+++ src/preprocess.lisp
@@ -10,31 +10,37 @@
   (format nil "~A -xc - -E"
           +cc+))
 
-(defmacro with-command (cmd input &key on-error &rest body)
+(defmacro with-command ((cmd input &key on-error) &rest body)
   `(multiple-value-bind (stdout stderr exit-code)
-       (trivial-shell:shell-command ,cmd :input ,data)
+       (trivial-shell:shell-command ,cmd :input ,input)
      (when (not (eql exit-code 0))
        ,on-error)
      ,@body))
 
 (defun preprocess (data)
-  (with-command 
-    +preproc-cmd+
-    data
-    :on-error (progn
-                (format t "An error occurred during preprocessing:~&")
-                (format t "~A" stderr)
-                (sb-ext:quit))
+  (with-command (+preproc-cmd+
+                 data
+                 :on-error (progn
+                             (format t "An error occurred during preprocessing:~&")
+                             (format t "~A" stderr)
+                             (sb-ext:quit)))
     stdout))
 
-(defparameter +cmc-lexer-bin+ "/usr/bin/cmc-lexer")
+(defparameter +cmc-lexer-bin+
+  (first
+   (remove-if #'null
+              (mapcar #'probe-file
+                      (list #p"/usr/bin/cmc-lexer"
+                            (merge-pathnames
+                             #p"grammar/cmc-lexer"
+                             (asdf:component-pathname (asdf:find-system :cmacro))))))))
 
 (defun lex (data)
-  (with-command 
-    +cmc-lexer-bin+
-    data
-    (progn
-      (format t "An error occurred during lexing:~&")
-      (format t "~A" stderr)
-      (sb-ext:quit))
+  (with-command (+cmc-lexer-bin+
+                 data
+                 :on-error
+                 (progn
+                   (format t "An error occurred during lexing:~&")
+                   (format t "~A" stderr)
+                   (sb-ext:quit)))
     stdout))
