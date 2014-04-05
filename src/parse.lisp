7fdcd6114c55c87241a91c6d498899ed05ace748
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-05 18:18:14 -0300

    Add parsing function

--- src/parse.lisp
+++ src/parse.lisp
@@ -18,23 +18,41 @@
   (cdr (assoc tok-type +token-type-map+ :test #'equal)))
 
 (defparameter +separator-map+
-  '(("(" . :list-open)
-    (")" . :list-close)
-    ("[" . :array-open)
-    ("]" . :array-close)
-    ("{" . :block-open)
-    ("}" . :block-close)))
+  '(("(" . (:list . :open))
+    (")" . (:list . :close))
+    ("[" . (:array . :open))
+    ("]" . (:array . :close))
+    ("{" . (:block . :open))
+    ("}" . (:block . :close))))
 
 (defun map-separator (sep)
   (cdr (assoc sep +separator-map+ :test #'equal)))
 
-(defun process (data)
+(defun process (lexemes)
   (mapcar 
    #'(lambda (lexeme)
        (let ((tok-type (map-token-type (subseq lexeme 0 3)))
              (tok-text (subseq lexeme 4)))
-         (aif (and (eql tok-type :op)
+         (aif (and (eq tok-type :op)
                    (map-separator tok-text))
               (make-token :type :op :text it)
               (make-token :type tok-type :text tok-text))))
-   data))
+   lexemes))
+
+(defun parse (tokens)
+  (let ((context (list nil)))
+    (loop for tok in tokens do
+      (if (listp (token-text tok))
+          ;; Separator token
+          (if (eq :open (cdr (token-text tok)))
+              ;; Opening token
+              (push (list) context)
+              ;; Closing token
+              (let ((cur-context (pop context)))
+                (setf (first context)
+                      (append (first context) (list cur-context)))))
+          ;; Common token
+          (setf (first context)
+                (append (first context)
+                        (list tok)))))
+    context))
