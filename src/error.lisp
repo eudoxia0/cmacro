49f539c92b742e762e043c0ac26b88c1dc46dafb
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-15 19:51:12 -0300

    Quit on errors

--- src/error.lisp
+++ src/error.lisp
@@ -4,20 +4,25 @@
   (:import-from :cmacro.parse
                 :token-text)
   (:export :bad-macro-definition
-           :bad-match))
+           :bad-match
+           :no-input-files))
 (in-package :cmacro.error)
+
+(setf *debugger-hook* #'(lambda (c h) (format t "~A~&" c) (sb-ext:quit)))
+
+(define-condition cmacro-error () ())
 
 (defparameter +bad-match-msg+
   "Error trying to macroexpand '~A': The input didn't match any cases.")
 
-(define-condition bad-macro-definition ()
+(define-condition bad-macro-definition (cmacro-error)
   ((text :initarg :text :reader text))
 
   (:report
    (lambda (condition stream)
      (format stream "Bad macro definition: ~A." (text condition)))))
 
-(define-condition bad-match ()
+(define-condition bad-match (cmacro-error)
   ((token :initarg :token :reader token))
 
   (:report
@@ -26,3 +31,8 @@
        (format stream
                +bad-match-msg+
                (token-text token))))))
+
+(define-condition no-input-files (cmacro-error)
+  ()
+  (:report (lambda (condition stream)
+             (format stream "No input files."))))
