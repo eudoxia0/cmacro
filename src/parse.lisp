bb873d83043dcc51c248369e5c62ff9a14b7d173
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-16 19:28:39 -0300

    Add JSON import

--- src/parse.lisp
+++ src/parse.lisp
@@ -156,5 +156,16 @@
     (encode ast stream)
     (get-output-stream-string stream)))
 
+(defun hash-to-token (hash-table)
+  (make-token :type (intern (gethash "type" hash-table)
+                            (find-package :keyword))
+              :text (gethash "text" hash-table)))
+
+(defun import-sexp (sexp)
+  (loop for node in sexp collecting
+        (if (listp node)
+            (import-sexp node)
+            (hash-to-token node))))
+
 (defun json-to-ast (string)
-  (yason:parse string))
+  (import-sexp (yason:parse string)))
