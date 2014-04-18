9073912fd52cfb5b8cbccfc92f09836c12177430
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-18 10:43:10 -0300

    Change stream to buffer, clean up some code

--- src/print.lisp
+++ src/print.lisp
@@ -1,36 +1,54 @@
 (in-package :cmacro.parse)
 
-(defun space-after-last-char-p (stream)
-  "Take a stream, and determine whether we should print a space before the next
-token."
-  t)
+;; Utility functions
 
-(defun print-token (token stream)
-  (if (and (not (separator-token-p token))
-           (not (parenp token))
-           (space-after-last-char-p stream))
-    (write-char #\Space stream))
-  ;; If it's a curly brace, also print a space before it
-  (if (blockp token)
-      (write-char #\Space stream))
-  (write-string (token-text token)
-                stream)
+(defun make-string-buffer ()
+  (make-array 0 :fill-pointer 0
+                :adjustable t
+                :element-type 'character))
+
+(defun buffer-write-char (buf char)
+  (vector-push-extend char buf))
+
+(defun buffer-write-string (buf string)
+  (let ((rest-string (subseq string 1)))
+    (vector-push-extend (elt string 0) buf (1+ (length string)))
+    (loop for char across rest-string do
+      (vector-push char buf))))
+
+(defun space-after-last-char-p (buffer)
+  "Take a buffer, and determine whether we should print a space before the next
+token."
+  (if (> (length buffer) 0)
+      (let ((last-char (elt buffer (1- (length buffer)))))
+        t)))
+
+(defun print-token (token buffer)
+  (if (or (identp token)
+          ;; If it's a curly brace, also print a space before it
+          (blockp token)
+          (and (not (separator-token-p token))
+               (not (parenp token))
+               (space-after-last-char-p buffer)))
+    (buffer-write-char buffer #\Space))
+  (buffer-write-string buffer
+                       (token-text token))
   (when (or (blockp token)
             (and (eq (token-type token) :op)
                  (equal (token-text token) ";")))
-    (write-char #\Newline stream)))
+    (buffer-write-char buffer #\Newline)))
 
-(defun print-expression (expression stream)
-  "Print an AST into a given stream."
+(defun print-expression (expression buffer)
+  "Print an AST into a given buffer."
   (if (listp expression)
       ;; Block
       (progn
         ;; Print the separator, then, if it's a curly brace, print
         ;; a newline
         (let ((sep (car expression)))
-          (print-expression sep stream)
+          (print-expression sep buffer)
           (loop for item in (cdr expression) do
-            (print-expression item stream))
+            (print-expression item buffer))
           ;; Print the matching closing separator
           (aif (and (token-p sep)
                     (position (token-text sep)
@@ -38,12 +56,12 @@ token."
                               :test #'equal))
                (print-expression
                 (make-token :type :op :text (nth it +closing-separators+))
-                stream))))
+                buffer))))
       ;; Regular token
-      (print-token expression stream)))
+      (print-token expression buffer)))
 
 (defun print-ast (ast)
   "Turn an AST into a list."
-  (let ((stream (make-string-output-stream)))
-    (print-expression ast stream)
-    (get-output-stream-string stream)))
+  (let ((buffer (make-string-buffer)))
+    (print-expression ast buffer)
+    buffer))
