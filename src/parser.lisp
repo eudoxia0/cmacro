653e37b7b4f8348b40b9328091216636fcebb488
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-17 13:30:51 -0300

    Add numbers stuff

--- src/parser.lisp
+++ src/parser.lisp
@@ -3,13 +3,42 @@
   (:use :cl :esrap))
 (in-package :cmacro.parser)
 
-(defrule octal-digits (or #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7))
-(defrule dec-digit (or #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9))
-(defrule hex-digit (or dec-digit
-                       #\a #\A #\b #\B #\c #\C #\d #\D #\e #\E #\f #\F))
+;;; Whitespace
 
 (defrule whitespace (+ (or #\Space #\Tab #\Newline #\Linefeed))
   (:constant nil))
+
+;;; Numbers
+
+(defrule octal-digit (or #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7))
+
+(defrule dec-digit (or #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9))
+
+(defrule hex-digit (or dec-digit
+                       #\a #\A #\b #\B #\c #\C #\d #\D #\e #\E #\f #\F))
+
+(defrule integer-suffix (or (and (or #\u #\U)
+                                 (? (or #\l #\L
+                                        (and #\l #\l)
+                                        (and #\L #\L))))
+                            (and (or #\l #\L
+                                     (and #\l #\l)
+                                     (and #\L #\L)))
+                                 (? (or #\u #\U)))
+  (:constant nil))
+
+(defrule float-suffix (or #\f #\F #\l #\L)
+  (:constant nil))
+
+(defrule octal (and #\0 (+ octal-digit))
+  (:destructure (o digits)
+    (text o digits)))
+
+(defrule hex (and #\0 (or #\x #\X) (+ hex-digit))
+  (:destructure (o x digits)
+    (text o x digits)))
+
+;;; Strings
 
 (defun not-doublequote (char)
   (not (eql #\" char)))
