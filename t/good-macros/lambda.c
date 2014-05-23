ec310d8a8f52b91d159c5932041e184ff9de3458
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-22 23:10:40 -0300

    Make the lambda example more interesting

--- t/good-macros/lambda.c
+++ t/good-macros/lambda.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 macro lambda {
   case {
     match {
@@ -13,5 +16,13 @@ macro lambda {
 }
 
 int main() {
-  lambda (int a, int b) -> int { return a + b; };
+  int array[] = {423, 61, 957, 133, 969, 829, 821, 390, 704, 596};
+  
+  qsort(array, 10, sizeof(int),
+        lambda (const void* a, const void* b) -> int
+        { return *(int*)a - *(int*)b; });
+  for(size_t i = 0; i < 10; i++){
+    printf("%i ", array[i]);
+  }
+  return 0;
 }
