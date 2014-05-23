27a1e9bdfae9841cc4f85a907119059e96e870d0
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-22 23:10:22 -0300

    Update template commands

--- src/template.lisp
+++ src/template.lisp
@@ -3,42 +3,28 @@
   (:use :cl :anaphora)
   (:import-from :cmacro.token
                 :<identifier>
+                :<string>
                 :<variable>
                 :var-name
                 :var-p
                 :var-qualifiers
-                :var-command-p)
+                :var-command-p
+                :make-variable)
+  (:import-from :cmacro.printer
+                :print-ast)
   (:import-from :cmacro.db
                 :gen-sym
                 :get-sym)
   (:export :render-template))
 (in-package :cmacro.template)
 
-#|
-(defun run-template-command (command args)
-  (cond
-    ((equal command "gensym")
-     ;; Generate a symbol, and associate it with a label
-     (cmacro.db:gen-sym (first args)))
-    ((equal command "getsym")
-     ;; Get a symbol from a label, an optional `n` places back
-     (aif (second args)
-          (cmacro.db:get-sym (first args) (parse-integer (second args)))
-          (cmacro.db:get-sym (first args))))
-    ((equal command "to-string")
-     (format nil "~S" (get-var-value (first args))))
-    ((equal command "splice")
-     (let ((block (first (cmacro.parse:parse-data (get-var-value (first args))))))
-       (print block)
-       (cmacro.parse:print-ast
-        (if (listp block)
-            (append (list (cmacro.parse:make-token :type :op :text ""))
-                    (rest block))
-            block))))
-    ((equal command "embed")
-     (format nil "$(~{~A ~})" args))
-    (t
-     (error 'cmacro.error:unknown-template-command :command command))))|#
+(defmethod get-var ((var <variable>) bindings)
+  (gethash (var-name var) bindings))
+
+(defmethod render-var ((var <variable>) bindings)
+  (aif (get-var var bindings)
+       it
+       (error 'cmacro.error:unknown-var :var-name (var-name var))))
 
 (defun render-command (command args bindings)
   (cond
@@ -52,16 +38,19 @@
                     (aif (second args)
                          (get-sym (first args) (parse-integer (second args)))
                          (get-sym (first args)))))
+    ((equal command "@to-string")
+     (make-instance '<string>
+                    :text (concatenate 'string
+                                       "\""
+                                       (print-ast
+                                        (get-var (make-variable (first args))
+                                                 bindings))
+                                       "\"")))
+    ((equal command "@embed")
+     (make-instance '<identifier>
+                    :text (format nil "$(~{~A~#[~:; ~]~})" args)))
     (t
      (error 'cmacro.error:unknown-template-command :command command))))
-
-(defmethod get-var ((var <variable>) bindings)
-  (gethash (var-name var) bindings))
-
-(defmethod render-var ((var <variable>) bindings)
-  (aif (get-var var bindings)
-       it
-       (error 'cmacro.error:unknown-var :var-name (var-name var))))
 
 (defmethod render-template-expression ((var <variable>) bindings)
   (if (var-command-p var)
