5f45459fbec48080bf76234761cbc0171bcce523
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-24 22:53:56 -0300

    Add with-open-file example

---
+++ t/good-macros/with-open-file.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+macro withOpenFile {
+  case {
+    match {
+      ($(stream-name), $(pathname string), $(direction string))
+        $(body block)
+    }
+    template {
+      {
+        FILE* $(stream-name) = fopen($(pathname), $(direction));
+        if($(stream-name))
+          $(body)
+        fclose($(stream-name));
+      }
+    }
+  }
+}
+
+int main() {
+  withOpenFile(file, "README.md", "r") {
+    printf("The first character in the README is: %c\n",
+           fgetc(file));
+  }
+  return 0;
+}
