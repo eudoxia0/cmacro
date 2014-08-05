1f7d56e2da0ff7325c2943e96a1b71cf228cff7c
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-05 15:52:52 -0300

    Restrain some types

--- src/db.lisp
+++ src/db.lisp
@@ -5,16 +5,18 @@
            :get-sym))
 (in-package :cmacro.db)
 
-
 (defparameter *symbol-index* 0)
 
 (defun make-sym (label n)
+  (declare (type string label) (type integer n))
   (format nil "cmacro_~A_~A" label n))
 
 (defun gen-sym (label)
+  (declare (type string label))
   (make-sym label (incf *symbol-index*)))
 
 (defun get-sym (label &optional (n 0))
+  (declare (type string label) (type integer n))
   (if (< (- *symbol-index* n) 1)
       (make-sym label 0)
       (make-sym label (- *symbol-index* n))))
