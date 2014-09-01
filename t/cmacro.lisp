936c4020b4ef61122e4deaaf2ada09a5b6642e2d
Fernando Borretti <eudoxiahp@gmail.com>
2014-09-01 18:31:53 -0300

    Indentation fixes

--- t/cmacro.lisp
+++ t/cmacro.lisp
@@ -29,34 +29,22 @@
 (in-suite parser)
 
 (test integer-parsing
-  (is
-   (equal "0123"
-          (parse 'cmacro.parser::octal "0123")))
-  (is
-   (equal "0x123"
-          (parse 'cmacro.parser::hex "0x123")))
-  (is
-   (equal "123"
-          (parse 'cmacro.parser::dec "123")))
-  (is (equal "123"
-             (token-text (parse 'cmacro.parser::integer "123")))))
+  (is (equal "0123" (parse 'cmacro.parser::octal "0123")))
+  (is (equal "0x123" (parse 'cmacro.parser::hex "0x123")))
+  (is (equal "123" (parse 'cmacro.parser::dec "123")))
+  (is (equal "123" (token-text (parse 'cmacro.parser::integer "123")))))
 
 (test string-parsing
-  (is
-   (equal "\"test\""
-          (token-text (parse 'cmacro.parser::string "\"test\""))))
-  (is
-   (equal "\"test \\\"inner string\\\"  test\""
-          (token-text (parse 'cmacro.parser::string
-                             "\"test \\\"inner string\\\"  test\"")))))
+  (is (equal "\"test\"" (token-text (parse 'cmacro.parser::string "\"test\""))))
+  (is (equal "\"test \\\"inner string\\\"  test\""
+             (token-text (parse 'cmacro.parser::string 
+                                "\"test \\\"inner string\\\"  test\"")))))
 
 (test identifier-parsing
-  (is
-   (equal "test"
-          (token-text (parse 'cmacro.parser::identifier "test"))))
-  (is
-   (equal "c_function01"
-          (token-text (parse 'cmacro.parser::identifier "c_function01")))))
+  (is (equal "test"
+             (token-text (parse 'cmacro.parser::identifier "test"))))
+  (is (equal "c_function01"
+             (token-text (parse 'cmacro.parser::identifier "c_function01")))))
 
 (def-suite pattern-matcher
   :description "Pattern matching tests.")
